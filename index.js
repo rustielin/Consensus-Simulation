848cff4503a0f55599b2ad9ecc9c48268a6ccb8a
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2019-02-18 23:56:13 -0800

    MESSAGING WORKS PER ROOM

--- index.js
+++ index.js
@@ -17,49 +17,56 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-// io.on('connection', socket => {
+io.on('connection', socket => {
 
-//   io.emit('user_join', socket.id);
+  io.emit('user_join', socket.id);
 
-//   // broadcast message to everyone
-//   socket.on('message', msg => { 
-//     console.log('message: ' + msg);
+  socket.on('join', function(room) {
+    socket.join(room);
+  });
 
-//     // TODO: boradcast only to those directly connected
-//     // TODO: don't broadcast to selt, act on local data
-//     // TODO: enable nodes to replay network state
-//     // TODO: probably define these message headers as enums
-//     io.emit('message', msg);
-//   });
+  // broadcast message to everyone
+  socket.on('message', msg => { 
+    console.log('message: ' + msg);
 
-//   socket.on('peers', msg => { // accepting peers list from other node
-//     var recipient = msg.recipient;
-//     var peers = msg.peers;
-//     console.log('got peers call, sending to ' + recipient);
-//     io.to(recipient).emit('peers', peers);
-//   });
-// });
+    // TODO: boradcast only to those directly connected
+    // TODO: don't broadcast to selt, act on local data
+    // TODO: enable nodes to replay network state
+    // TODO: probably define these message headers as enums
+    var id = msg.id;
+    var message = msg.message;
+    var room = msg.room;
+    io.to(room).emit('message', id + ': ' + message);
+  });
+
+  socket.on('peers', msg => { // accepting peers list from other node
+    var recipient = msg.recipient;
+    var peers = msg.peers;
+    console.log('got peers call, sending to ' + recipient);
+    io.to(recipient).emit('peers', peers);
+  });
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/register/:id', function(req, res){
-  var id = req.params.id;
-  console.log('got id: ' + id);
-  var nsp = io.of('/' + id);
-  nsp.on('connection', function(socket){
-    console.log('someone connected');
+// app.get('/register/:id', function(req, res){
+//   var id = req.params.id;
+//   console.log('got id: ' + id);
+//   var nsp = io.of('/' + id);
+//   nsp.on('connection', function(socket){
+//     console.log('someone connected');
 
-    socket.on('message', msg => {
-      console.log('message: ' + msg);
-      nsp.emit('message', msg);
-    });
+//     socket.on('message', msg => {
+//       console.log('message: ' + msg);
+//       nsp.emit('message', msg);
+//     });
 
-  });
+//   });
 
-  res.sendFile(__dirname + '/index.html');
-});
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 // // An api endpoint that returns a short list of items
 // app.get('/api/getList', (req, res) => {
