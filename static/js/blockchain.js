fbb54f8db60160c20b98f55142378e4e04f01bc8
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2019-04-26 21:37:54 -0700

    resolve fork

--- static/js/blockchain.js
+++ static/js/blockchain.js
@@ -3,15 +3,26 @@ var blockchain = {
 	consensus: "proof of stake",
 	length: 0,
 	blocks: [],
-	createBlock: function() {
-		var block = { 
-			index: this.length + 1,
-			value: "dis new block"
-		},
-		this.length = this.length + 1,
-		this.blocks.push(block)
-		return block
-	}
 };
 
+var proposeBlock = (blockchain) => {
+	var block = {
+		creator: msg.sender,
+		index: blockchain.length + 1,
+		value: "dis new block"
+	};
+	blockchain.length = blockchain.length + 1;
+	blockchain.blocks.push(block);
+	return blockchain
+};
 
+var propagateBlock = (other_blockchain) => {
+	// new blockchain: user_blockchain
+	// old blockchain: blockchain
+	if (other_blockchain.length > blockchain.length) {
+		blockchain = other_blockchain;
+		peers.foreach(peer => {
+			socket.emit('received_blockchain', blockchain);
+		});
+	} 
+};
